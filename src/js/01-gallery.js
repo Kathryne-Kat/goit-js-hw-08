682582c97651bfdf497aba6efa4cab6e951cc74d
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2022-12-28 22:47:50 +0200

    add #1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox"; // Описаний в документації
+import "simplelightbox/dist/simple-lightbox.min.css";// Додатковий імпорт стилів
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListMarkup = document.querySelector('.gallery');
+
+const makeGalleryCard = ({ preview, description, original } = galleryItems) => {
+    return ` 
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+};
+const galleryCards = galleryItems.map((el) => {
+    return makeGalleryCard(el);
+});
+galleryListMarkup.insertAdjacentHTML('afterbegin', galleryCards.join('')); 
+
+const lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionPosition: 'bottom',
+        captionDelay: 250,
+    }); 
