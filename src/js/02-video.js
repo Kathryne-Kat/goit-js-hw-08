2787c5fa1974056f9973b47415d6843408734dae
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2022-12-30 00:31:45 +0200

    add #2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,19 +5,12 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
 const onPlay = function (data) {
-    let time = data.seconds;
-    localStorage.setItem('videoplayer-current-time', time);
-    console.log('played the video!');
+    //console.log(data);
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    //console.log('played the video!');
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
