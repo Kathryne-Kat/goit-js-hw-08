593ee7e4b5a7502a7c54b9352e9ef4f05fc136b4
Kathryne-Kov <112563496+Kathryne-Kat@users.noreply.github.com>
2022-12-30 12:02:12 +0200

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -13,4 +13,18 @@ const onPlay = function (data) {
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+let time = localStorage.getItem('videoplayer-current-time');
+console.log(time);
+player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
